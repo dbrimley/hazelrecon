f33c3f9ce88acd70cc7fae974682f7b994044063
dbrimley <david@hazelcast.com>
2014-11-26 08:54:36 +0000

    More fault aware RepManagerEnabledClusterMember

--- reconframework/src/main/java/com/craftedbytes/hazelcast/wan/RepManagerEnabledClusterMember.java
+++ reconframework/src/main/java/com/craftedbytes/hazelcast/wan/RepManagerEnabledClusterMember.java
@@ -5,31 +5,57 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 
+import java.io.InputStream;
+
 /**
  * Starts a Hazelcast Cluster node that contains a Replication Manager
+ * Requires Environment variable LOCATION to be set and a replication map to be present in xml config
  */
 public class RepManagerEnabledClusterMember {
 
+    public static final String REPLICATION_MAP = "replication";
+    public static final String LOCATION_SYSTEM_PROPERTY = "LOCATION";
     private static HazelcastInstance hazelcastInstance;
 
     public static void main(String args[]){
-        hazelcastInstance = getLocationAwareHazelcastInstance();
-        initReplication();
+        try {
+            hazelcastInstance = getLocationAwareHazelcastInstance();
+            initReplication();
+        } catch(RuntimeException e){
+            System.out.println(e);
+            System.exit(-1);
+        }
     }
 
     private static void initReplication() {
-
-        IMap replicationMap = hazelcastInstance.getMap("replication");
+        IMap replicationMap = hazelcastInstance.getMap(REPLICATION_MAP);
         RepManager cluster1RepManager = new RepManager(hazelcastInstance,replicationMap);
         cluster1RepManager.start();
-
     }
 
     private static HazelcastInstance getLocationAwareHazelcastInstance() {
-        String location = System.getProperty("LOCATION");
-        XmlConfigBuilder xmlConfigBuilder = new XmlConfigBuilder(RepManagerEnabledClusterMember.class.getResourceAsStream(
-                "/" + location + ".hazelcast.xml"));
+        String location = getLocationEnvironmentVariable();
+        InputStream inputStream = getLocationAwareXMLConfig(location);
+        XmlConfigBuilder xmlConfigBuilder = new XmlConfigBuilder(inputStream);
         return Hazelcast.newHazelcastInstance(xmlConfigBuilder.build());
+    }
+
+    private static InputStream getLocationAwareXMLConfig(String location) {
+        String xmlLocation = "/" + location + ".hazelcast.xml";
+        InputStream inputStream = RepManagerEnabledClusterMember.class
+                .getResourceAsStream(xmlLocation);
+        if (inputStream == null){
+            throw new RuntimeException("Could not find inputStream  " + xmlLocation);
+        }
+        return inputStream;
+    }
+
+    private static String getLocationEnvironmentVariable() {
+        String location = System.getProperty(LOCATION_SYSTEM_PROPERTY,null);
+        if (location == null){
+            throw new RuntimeException("Could not find environment variable " + LOCATION_SYSTEM_PROPERTY);
+        }
+        return location;
     }
 
 }
